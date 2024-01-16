106264c25ae6ec2387c9f8836cfeca6b2b3b2b9c
DESKTOP-4OKI60B\trid2 <trid2300@gmail.com>
2024-01-16 17:17:57 +0700

    pull

---
+++ stationeryManagement/Migrations/20240116095347_v4.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace stationeryManagement.Migrations
+{
+    public partial class v4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Suppliers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Stationeries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Imports",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Stationeries");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Imports");
+
+            migrationBuilder.DropColumn(
+                name: "Deleted",
+                table: "Categories");
+        }
+    }
+}
